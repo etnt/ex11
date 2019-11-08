2ab896f7c25d92524797ac9135b2ca9ecf80a98f
Torbjorn Tornkvist <ttornkvi@cisco.com>
2019-11-08 16:44:44 +0100

    Removing Tabs and Spaces

--- widgets/swLabel.erl
+++ widgets/swLabel.erl
@@ -11,20 +11,20 @@
 
 -include("sw.hrl").
 
--import(ex11_lib, [ePolyText8/5, rpc/2, sleep/1, 
-		   xClearArea/1,
-		   xDo/2, xFlush/1,
-		   xVar/2]).
+-import(ex11_lib, [ePolyText8/5, rpc/2, sleep/1,
+                   xClearArea/1,
+                   xDo/2, xFlush/1,
+                   xVar/2]).
 
-make(Parent, X, Y, Width, Ht, Border, Color, Str) -> 
+make(Parent, X, Y, Width, Ht, Border, Color, Str) ->
     spawn_link(fun() -> init(Parent,  X, Y, Width, Ht, Border, Color,Str) end).
 
 init(Parent, X, Y, Width, Ht, Border, Color,Str) ->
     Display = rpc(Parent, display),
     PWin = rpc(Parent, mountPoint),
     Wargs = #win{x=X, y=Y, parent=PWin,
-		 border=Border,width=Width,ht=Ht,color=Color, 
-		 type=label, mask = ?EVENT_EXPOSURE},
+                 border=Border,width=Width,ht=Ht,color=Color,
+                 type=label, mask = ?EVENT_EXPOSURE},
     Wargs1 = sw:mkWindow(Display, Parent, Wargs),
     Win = Wargs1#win.win,
     Bin =  ePolyText8(Win, xVar(Display, sysFontId), 10, 18, Str),
@@ -32,23 +32,20 @@ init(Parent, X, Y, Width, Ht, Border, Color,Str) ->
 
 loop(B, Display, Wargs) ->
     receive
-	{event,_,expose, _} ->
-	    xDo(Display, B),
-	    xFlush(Display),
-	    loop(B, Display, Wargs);
-	{set, Str} ->
-	    Win = Wargs#win.win, 
-	    xDo(Display, xClearArea(Win)),
-	    Bin =  ePolyText8(Win, xVar(Display, sysFontId), 10, 18, Str),
-	    self() ! {event,void,expose,void},
-	    loop(Bin, Display, Wargs);
-	{'EXIT', Pid, Why} ->
-	    true;
-	Any ->
-	    %% Now we call the generic operators 
-	    Wargs1 = sw:generic(Any, Display, Wargs),
-	    loop(B, Display, Wargs1)
+        {event,_,expose, _} ->
+            xDo(Display, B),
+            xFlush(Display),
+            loop(B, Display, Wargs);
+        {set, Str} ->
+            Win = Wargs#win.win,
+            xDo(Display, xClearArea(Win)),
+            Bin =  ePolyText8(Win, xVar(Display, sysFontId), 10, 18, Str),
+            self() ! {event,void,expose,void},
+            loop(Bin, Display, Wargs);
+        {'EXIT', Pid, Why} ->
+            true;
+        Any ->
+            %% Now we call the generic operators
+            Wargs1 = sw:generic(Any, Display, Wargs),
+            loop(B, Display, Wargs1)
     end.
-
-
-
