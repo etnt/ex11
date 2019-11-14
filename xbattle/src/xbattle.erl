1db95aa0e01a1f54c317e9f84956d5ccd894f5ce
Torbjorn Tornkvist <ttornkvi@cisco.com>
2019-11-14 15:40:57 +0100

    Reorganize cell record handling

--- xbattle/src/xbattle.erl
+++ xbattle/src/xbattle.erl
@@ -20,34 +20,44 @@
 
 -define(bg, 16#ffffcc).
 
--record(cell, {
-          type = square,
-          size = 60
+-record(cell_square, {
+          size = 60,
+
+          origo_x,
+          origo_y,
+          radius,
+
+          north_pid,
+          east_pid,
+          south_pid,
+          west_pid
          }).
 
 -record(board, {
           width  = 660,
           height = 660,
-          cell   = #cell{}
+          cell_type = square,
+          cell
          }).
 
 -define(xbattle, xbattle).
 
 new_board() ->
     ets:new(?xbattle, [set,protected,named_table,{read_concurrency,true}]),
-    adjust_board_size(#board{}).
+    #board{}.
 
 %% Adjust the board size to fit the cells.
 adjust_board_size(#board{width  = W,
                          height = H,
-                         cell   = #cell{type = square,
-                                        size = Size}} = B) ->
+                         cell   = #cell_square{size = Size}} = B) ->
 
     Width  = (W div Size) * Size,
     Height = (H div Size) * Size,
 
     B#board{width  = Width,
             height = Height}.
+
+new_cell(#board{cell_type = square}) -> #cell_square{}.
 
 
 
@@ -58,10 +68,16 @@ start() ->
 win() ->
     process_flag(trap_exit, true),
 
-    B       = new_board(),
+    Board0  = new_board(),
+    Cell    = new_cell(Board0),
+    Board   = adjust_board_size(Board0#board{cell = Cell}),
+
     Display = xStart("3.2"),
-    Win     = swTopLevel:make(Display, B#board.width, B#board.height, ?bg),
-    Canvas  = swCanvas:make(Win,0,0,B#board.width,B#board.height,1,?gray88),
+    Win     = swTopLevel:make(Display,Board#board.width,Board#board.height,?bg),
+    Canvas  = swCanvas:make(Win,0,0,
+                            Board#board.width,
+                            Board#board.height,
+                            1,?gray88),
     Self    = self(),
 
     Canvas ! {onClick, fun({_,X,Y,_,_}) ->
@@ -77,32 +93,32 @@ win() ->
                              Self ! {key, Cmd, {Kx,Ky}}
                      end},
 
-    draw_board(Canvas, B),
+    draw_board(Canvas, Board, Cell),
 
     start_cell_processes(),
 
-    loop(Canvas, B).
+    loop(Canvas, Board).
 
 
-loop(Canvas, B) ->
+loop(Canvas, Board) ->
     receive
 
         {click, Wx, Wy} = Msg ->
-            forward_msg(B, Wx, Wy, Msg),
-            loop(Canvas, B);
+            forward_msg(Board, Wx, Wy, Msg),
+            loop(Canvas, Board);
 
         {key, _KeyInfo, {Wx, Wy}} = Msg->
-            forward_msg(B, Wx, Wy, Msg),
-            loop(Canvas, B);
+            forward_msg(Board, Wx, Wy, Msg),
+            loop(Canvas, Board);
 
         Any ->
             ?dbg("received:~p~n",[Any]),
-            loop(Canvas, B)
+            loop(Canvas, Board)
 
     end.
 
-forward_msg(B, Wx, Wy, Msg) ->
-    Point = cell_coord(B, Wx, Wy),
+forward_msg(Board, Wx, Wy, Msg) ->
+    Point = cell_coord(Board, Wx, Wy),
     [{_,CellPid}] = ets:lookup(?xbattle, Point),
     CellPid ! Msg.
 
@@ -115,14 +131,15 @@ start_cell_processes() ->
               end, ok, ?xbattle).
 
 
-draw_board(Canvas, #board{width = Width, height = Height, cell = Cell} = B) ->
+draw_board(Canvas, #board{width = Width, height = Height} = B, Cell) ->
     newPen(Canvas, thin, ?black, 1),
     newPen(Canvas, blue, ?blue, 1),
 
     draw_cells(Canvas, B, Width, Height, Cell).
 
 
-draw_cells(Canvas, Board, Width, Height, #cell{type = square, size = Size}) ->
+draw_cells(Canvas, Board, Width, Height,
+           #cell_square{size = Size} = Cell) ->
 
     Yinit     = 0,
     MaxWidth  = Width,
@@ -130,39 +147,40 @@ draw_cells(Canvas, Board, Width, Height, #cell{type = square, size = Size}) ->
 
     for(Yinit, MaxHeight, Size,
         fun(Y) ->
-                draw_row(Canvas, Board, Y, MaxWidth, Size, Size)
+                draw_row(Canvas, Board, Cell, Y, MaxWidth, Size, Size)
         end).
 
 
-draw_row(Canvas, Board, Y, MaxWidth, Width, Height) ->
+draw_row(Canvas, Board, Cell, Y, MaxWidth, Width, Height) ->
 
     for(0, MaxWidth, Width,
         fun(X) ->
                 draw(Canvas, thin, {rectangle, X, Y, Width, Height}),
 
                 %% Create the Cell Process
-                setup_cell_proc(Canvas, Board, Width, Height, X, Y),
+                setup_cell_proc(Canvas, Board, Cell, Width, Height, X, Y),
 
                 %% Write out some Cell pos info!
                 dbg_cell_pos(Canvas, Width, Height, X, Y)
         end).
 
-setup_cell_proc(Canvas, Board, Width, Height, X, Y) ->
+setup_cell_proc(Canvas, Board, Cell, Width, Height, X, Y) ->
     Self = self(),
     CellCoord = cell_coord(Width, Height, X, Y),
     CellPid = spawn_link(fun() ->
-                                 cell_init(Self, Canvas, Board, CellCoord)
+                                 cell_init(Self, Canvas, Board, Cell, CellCoord)
                          end),
     ets:insert(?xbattle, {CellCoord,CellPid}),
     put(CellPid, CellCoord).
+
+cell_coord(#board{cell = #cell_square{size = Size}} ,X, Y) ->
+    {cell_div(Y, Size),  % row
+     cell_div(X, Size)}. % col
 
 cell_coord(Width, Height, X, Y) ->
     {cell_div(Y,Height),
      cell_div(X,Width)}.
 
-cell_coord(#board{cell = #cell{type = square, size = Size}} ,X, Y) ->
-    {cell_div(Y, Size),  % row
-     cell_div(X, Size)}. % col
 
 dbg_cell_pos(Canvas, Width, Height, X, Y) ->
     CoordStr = lists:flatten(io_lib:format("~p,~p",[X,Y])),
@@ -185,30 +203,31 @@ for(I, Max, Step, F) ->
 %% C E L L   P R O C E S S
 %%
 
-cell_init(Controller, Canvas, Board, Point) ->
+cell_init(Controller, Canvas, Board, Cell0, {Row, Col} = Point) ->
     receive
         {Controller, start} ->
-            cell_loop(Controller, Canvas, Board, Point)
+            Cell = set_pump_pos(Cell0, Row, Col),
+            cell_loop(Controller, Canvas, Board, Cell, Point)
     end.
 
-cell_loop(Controller, Canvas, Board, {Row,Col} = Point) ->
+cell_loop(Controller, Canvas, Board, Cell, Point) ->
     receive
 
         {click, Wx, Wy} ->
             ?dbg("~p Clicked at: (~p,~p) is inside cell: ~p~n",
                  [self(), Wx, Wy, Point]),
-            animate_pump(Canvas, Board, Row, Col),
-            cell_loop(Controller, Canvas, Board, Point);
+            animate_pump(Canvas, Cell),
+            cell_loop(Controller, Canvas, Board, Cell, Point);
 
         {key, {_State,_Key,_Type,Val}, {_Wx, _Wy} = Pos} ->
             ?dbg("~p Got key: ~p at pos: ~p~n", [self(),Val,Pos]),
-            cell_loop(Controller, Canvas, Board, Point)
+            cell_loop(Controller, Canvas, Board, Cell, Point)
 
     end.
 
 
-animate_pump(Canvas, Board, Row, Col) ->
-    {Xorigo, Yorigo, Radius} = get_pump_pos(Board, Row, Col),
+animate_pump(Canvas, Cell) ->
+    {Xorigo, Yorigo, Radius} = get_pump_pos(Cell),
 
     Step = (Radius - 2) div 3,
 
@@ -222,12 +241,13 @@ draw_pump(Canvas, X, Y, Radius) ->
     draw(Canvas, blue, {filledCircle, X, Y, Radius}).
 
 
-get_pump_pos(#board{cell = #cell{type = square, size = Size}},
-             Row, Col) ->
+get_pump_pos(#cell_square{origo_x = X, origo_y = Y, radius = R}) ->
+    {X, Y, R}.
+
+set_pump_pos(#cell_square{size = Size} = Cell, Row, Col) ->
 
     Radius = Size div 2,
 
-    Xorigo = (Col * Size) + Radius,
-    Yorigo = (Row * Size) + Radius,
-
-    {Xorigo, Yorigo, Radius}.
+    Cell#cell_square{radius  = Radius,
+                     origo_x = (Col * Size) + Radius,
+                     origo_y = (Row * Size) + Radius}.
