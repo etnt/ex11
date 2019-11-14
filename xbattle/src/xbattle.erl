07356fbaadd02e02e16186c68b211678bf86d356
Torbjorn Tornkvist <ttornkvi@cisco.com>
2019-11-14 10:58:32 +0100

    Now, each cell is controlled by one process

--- xbattle/src/xbattle.erl
+++ xbattle/src/xbattle.erl
@@ -31,8 +31,10 @@
           cell   = #cell{}
          }).
 
+-define(xbattle, xbattle).
 
 new_board() ->
+    ets:new(?xbattle, [set,protected,named_table,{read_concurrency,true}]),
     adjust_board_size(#board{}).
 
 %% Adjust the board size to fit the cells.
@@ -51,9 +53,11 @@ adjust_board_size(#board{width  = W,
 
 
 start() ->
-    win().
+    spawn(fun() -> win() end).
 
 win() ->
+    process_flag(trap_exit, true),
+
     B       = new_board(),
     Display = xStart("3.2"),
     Win     = swTopLevel:make(Display, B#board.width, B#board.height, ?bg),
@@ -75,12 +79,136 @@ win() ->
 
     draw_board(Canvas, B),
 
+    start_cell_processes(),
+
     loop(Canvas, B).
 
 
+loop(Canvas, B) ->
+    receive
 
-animate_pump(Canvas, B, Row, Col) ->
-    {Xorigo, Yorigo, Radius} = get_pump_pos(B, Row, Col),
+        {click, Wx, Wy} = Msg ->
+            forward_msg(B, Wx, Wy, Msg),
+            loop(Canvas, B);
+
+        {key, _KeyInfo, {Wx, Wy}} = Msg->
+            forward_msg(B, Wx, Wy, Msg),
+            loop(Canvas, B);
+
+        Any ->
+            ?dbg("received:~p~n",[Any]),
+            loop(Canvas, B)
+
+    end.
+
+forward_msg(B, Wx, Wy, Msg) ->
+    Point = cell_coord(B, Wx, Wy),
+    [{_,CellPid}] = ets:lookup(?xbattle, Point),
+    CellPid ! Msg.
+
+
+start_cell_processes() ->
+    Self = self(),
+    ets:foldl(fun({{_Row,_Col},CellPid}, Acc) ->
+                      CellPid ! {Self,start},
+                      Acc
+              end, ok, ?xbattle).
+
+
+draw_board(Canvas, #board{width = Width, height = Height, cell = Cell} = B) ->
+    newPen(Canvas, thin, ?black, 1),
+    newPen(Canvas, blue, ?blue, 1),
+
+    draw_cells(Canvas, B, Width, Height, Cell).
+
+
+draw_cells(Canvas, Board, Width, Height, #cell{type = square, size = Size}) ->
+
+    Yinit     = 0,
+    MaxWidth  = Width,
+    MaxHeight = Height,
+
+    for(Yinit, MaxHeight, Size,
+        fun(Y) ->
+                draw_row(Canvas, Board, Y, MaxWidth, Size, Size)
+        end).
+
+
+draw_row(Canvas, Board, Y, MaxWidth, Width, Height) ->
+
+    for(0, MaxWidth, Width,
+        fun(X) ->
+                draw(Canvas, thin, {rectangle, X, Y, Width, Height}),
+
+                %% Create the Cell Process
+                setup_cell_proc(Canvas, Board, Width, Height, X, Y),
+
+                %% Write out some Cell pos info!
+                dbg_cell_pos(Canvas, Width, Height, X, Y)
+        end).
+
+setup_cell_proc(Canvas, Board, Width, Height, X, Y) ->
+    Self = self(),
+    CellCoord = cell_coord(Width, Height, X, Y),
+    CellPid = spawn_link(fun() ->
+                                 cell_init(Self, Canvas, Board, CellCoord)
+                         end),
+    ets:insert(?xbattle, {CellCoord,CellPid}),
+    put(CellPid, CellCoord).
+
+cell_coord(Width, Height, X, Y) ->
+    {cell_div(Y,Height),
+     cell_div(X,Width)}.
+
+cell_coord(#board{cell = #cell{type = square, size = Size}} ,X, Y) ->
+    {cell_div(Y, Size),  % row
+     cell_div(X, Size)}. % col
+
+dbg_cell_pos(Canvas, Width, Height, X, Y) ->
+    CoordStr = lists:flatten(io_lib:format("~p,~p",[X,Y])),
+    CellStr = lists:flatten(io_lib:format("~p,~p",
+                                          [cell_div(Y,Height),
+                                           cell_div(X,Width)])),
+    draw(Canvas, thin, {text, X, Y+20, CoordStr}),
+    draw(Canvas, thin, {text, X, Y+40, CellStr}).
+
+cell_div(_A,0) -> 0;
+cell_div(A,B)  -> A div B.
+
+
+for(I, Max, _Step, _F) when I >= Max ->
+    [];
+for(I, Max, Step, F) ->
+    [F(I)|for(I+Step,Max,Step,F)].
+
+%%
+%% C E L L   P R O C E S S
+%%
+
+cell_init(Controller, Canvas, Board, Point) ->
+    receive
+        {Controller, start} ->
+            cell_loop(Controller, Canvas, Board, Point)
+    end.
+
+cell_loop(Controller, Canvas, Board, {Row,Col} = Point) ->
+    receive
+
+        {click, Wx, Wy} ->
+            ?dbg("~p Clicked at: (~p,~p) is inside cell: ~p~n",
+                 [self(), Wx, Wy, Point]),
+            animate_pump(Canvas, Board, Row, Col),
+            cell_loop(Controller, Canvas, Board, Point);
+
+        {key, {_State,_Key,_Type,Val}, {_Wx, _Wy} = Pos} ->
+            ?dbg("~p Got key: ~p at pos: ~p~n", [self(),Val,Pos]),
+            cell_loop(Controller, Canvas, Board, Point)
+
+    end.
+
+
+animate_pump(Canvas, Board, Row, Col) ->
+    {Xorigo, Yorigo, Radius} = get_pump_pos(Board, Row, Col),
 
     Step = (Radius - 2) div 3,
 
@@ -103,79 +231,3 @@ get_pump_pos(#board{cell = #cell{type = square, size = Size}},
     Yorigo = (Row * Size) + Radius,
 
     {Xorigo, Yorigo, Radius}.
-
-
-
-
-draw_board(Canvas, #board{width = Width, height = Height, cell = Cell} = _B) ->
-    newPen(Canvas, thin, ?black, 1),
-    newPen(Canvas, blue, ?blue, 1),
-
-    draw_cells(Canvas, Width, Height, Cell).
-
-
-draw_cells(Canvas, Width, Height, #cell{type = square, size = Size}) ->
-
-    Yinit     = 0,
-    MaxWidth  = Width,
-    MaxHeight = Height,
-
-    for(Yinit, MaxHeight, Size,
-        fun(Y) ->
-                draw_row(Canvas, Y, MaxWidth, Size, Size)
-        end).
-
-
-draw_row(Canvas, Y, MaxWidth, Width, Height) ->
-
-    for(0, MaxWidth, Width,
-        fun(X) ->
-                draw(Canvas, thin, {rectangle, X, Y, Width, Height}),
-                %% Write out some Cell pos info!
-                dbg_cell_pos(Canvas, Width, Height, X, Y)
-        end).
-
-
-dbg_cell_pos(Canvas, Width, Height, X, Y) ->
-    CoordStr = lists:flatten(io_lib:format("~p,~p",[X,Y])),
-    CellStr = lists:flatten(io_lib:format("~p,~p",
-                                          [cell_div(Y,Height),
-                                           cell_div(X,Width)])),
-    draw(Canvas, thin, {text, X, Y+20, CoordStr}),
-    draw(Canvas, thin, {text, X, Y+40, CellStr}).
-
-cell_div(_A,0) -> 0;
-cell_div(A,B)  -> A div B.
-
-
-is_inside_cell(#board{cell = #cell{type = square, size = Size}} ,X, Y) ->
-    {cell_div(Y, Size),  % row
-     cell_div(X, Size)}. % col
-
-loop(Canvas, B) ->
-    receive
-
-        {click, Wx, Wy} ->
-            {Row, Col} = Point = is_inside_cell(B, Wx, Wy),
-            ?dbg("Clicked at: (~p,~p) is inside cell: ~p~n",
-                 [Wx, Wy, Point]),
-            spawn(fun() ->
-                          animate_pump(Canvas, B, Row, Col)
-                  end),
-            loop(Canvas, B);
-
-        {key, {_State,_Key,_Type,Val}, Pos} ->
-            ?dbg("Got key: ~p at pos: ~p~n", [Val,Pos]),
-            loop(Canvas, B);
-
-        Any ->
-            ?dbg("received:~p~n",[Any]),
-            loop(Canvas, B)
-
-    end.
-
-
-for(I, Max, _Step, _F) when I >= Max ->
-    [];
-for(I, Max, Step, F) ->
-    [F(I)|for(I+Step,Max,Step,F)].
